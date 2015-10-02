b3e1f03a06abd536e78e1cee33336581d25126c9
rick droogh <rak_keeper@hotmail.com>
2015-10-02 14:22:38 +0200

    Created assignment 5 end finished added some changes assignment 4

--- assignment4_exercise1-2_0911272.py
+++ assignment4_exercise1-2_0911272.py
@@ -1,4 +1,4 @@
-print("Witch exercise do u wanna see? 1, 2 or 3?")
+﻿print("Witch exercise do u wanna see? 1, 2 or 3?")
 sChoice = input("> ")
 if sChoice == 1:
    # a:
@@ -30,8 +30,18 @@ elif sChoice == 2:
     sPlayer1Question    = "What the choise of player 1? rock, paper, scissors, lizard or spock?"
     sPlayer2Question    = "What the choise of player 2? rock, paper, scissors, lizard or spock?"
     sDrawMessage        = "No one wins"
-    sPlayer1WinMessage  = "Player 1 wins"
-    sPlayer2WinMessage  = "Player 2 wins"
+    # sPlayer1WinMessage  = "Player 1 wins"
+    # sPlayer2WinMessage  = "Player 2 wins"
+    rock_paper          = "Paper covers rock"
+    rock_scissors       = "Rock crushes scissors"
+    rock_lizard         = "Rock crushes lizard"
+    rock_spock          = "Spock vaporizes rock"
+    paper_scissors      = "Scissors cuts paper"
+    paper_lizard        = "Lizard eats paper"
+    paper_spock         = "Paper disproves spock"
+    scissors_lizard     = "Scissors decapitates lizard"
+    scissors_spock      = "Spock smashes scissors"
+    lizard_spock        = "Lizard poisons spock"
     sMessage            = ""
 
     while sPlayer1choise == False or sPlayer2choise == False:
@@ -42,60 +52,60 @@ elif sChoice == 2:
             if sPlayer2choise == "rock":
                 sMessage = sDrawMessage
             elif sPlayer2choise == "paper":
-                sMessage = sPlayer2WinMessage
+                sMessage = rock_paper
             elif sPlayer2choise == "scissors":
-                sMessage = sPlayer1WinMessage
+                sMessage = rock_scissors
             elif sPlayer2choise == "lizard":
-                sMessage = sPlayer1WinMessage
+                sMessage = rock_lizard
             elif sPlayer2choise == "spock":
-                sMessage = sPlayer2WinMessage
+                sMessage = rock_spock
 
         elif sPlayer1choise == "paper":
             if sPlayer2choise == "rock":
-                sMessage = sPlayer1WinMessage
+                sMessage = rock_paper
             elif sPlayer2choise == "paper":
                 sMessage = sDrawMessage
             elif sPlayer2choise == "scissors":
-                sMessage = sPlayer2WinMessage
+                sMessage = paper_scissors
             elif sPlayer2choise == "lizard":
-                sMessage = sPlayer2WinMessage
+                sMessage = paper_lizard
             elif sPlayer2choise == "spock":
-                sMessage = sPlayer1WinMessage
+                sMessage = paper_spock
 
         elif sPlayer1choise == "scissors":
             if sPlayer2choise == "rock":
-                sMessage = sPlayer2WinMessage
+                sMessage = rock_scissors
             elif sPlayer2choise == "paper":
-                sMessage = sPlayer1WinMessage
+                sMessage = paper_scissors
             elif sPlayer2choise == "scissors":
                 sMessage = sDrawMessage
             elif sPlayer2choise == "lizard":
-                sMessage = sPlayer1WinMessage
+                sMessage = scissors_lizard
             elif sPlayer2choise == "spock":
-                sMessage = sPlayer2WinMessage
+                sMessage = scissors_spock
 
         # b
         elif sPlayer1choise == "lizard":
             if sPlayer2choise == "rock":
-                sMessage = sPlayer2WinMessage
+                sMessage = rock_lizard
             elif sPlayer2choise == "paper":
-                sMessage = sPlayer1WinMessage
+                sMessage = paper_lizard
             elif sPlayer2choise == "scissors":
-                sMessage = sPlayer2WinMessage
+                sMessage = scissors_lizard
             elif sPlayer2choise == "lizard":
                 sMessage = sDrawMessage
             elif sPlayer2choise == "spock":
-                sMessage = sPlayer1WinMessage
+                sMessage = lizard_spock
 
         elif sPlayer1choise == "spock":
             if sPlayer2choise == "rock":
-                sMessage = sPlayer1WinMessage
+                sMessage = rock_spock
             elif sPlayer2choise == "paper":
-                sMessage = sPlayer2WinMessage
+                sMessage = paper_spock
             elif sPlayer2choise == "scissors":
-                sMessage = sPlayer1WinMessage
+                sMessage = scissors_spock
             elif sPlayer2choise == "lizard":
-                sMessage = sPlayer2WinMessage
+                sMessage = lizard_spock
             elif sPlayer2choise == "spock":
                 sMessage = sDrawMessage
 
---
+++ assignment5_0911272/assignment5_0911272/assignment5_0911272.py
@@ -0,0 +1,47 @@
+﻿print "Which exercise do u wanna see? 1, 2 or 3?"
+
+iValue = input(">")
+if iValue == 1:
+    sValue = raw_input("What do u wanna reverse?")
+    begin = None
+    einde = None
+    reverse_string = slice(begin, einde, -1)
+    print sValue[reverse_string]
+elif iValue == 2:
+    import re
+    print "exersice 2"
+    sValue = raw_input("Choose a password!")
+    if re.match("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[&'(\-_)=~#{[|`\\^@\]}^$*¨£µ%,;:!?./§+]).{6,50}", sValue):
+        sMessage = "Password is strong!"         
+    elif re.match('(?=.*[a-z])(?=.*[0-9]).{6,50}', sValue):
+        sMessage = "Password is medium"
+    else:
+        sMessage = "Password is weak!"
+
+    print sMessage
+elif iValue == 3:
+    sCharacters     = raw_input("What letters do u want?")
+    iCount          = input("How much steps do u wanna jump in the alpabet?")
+    iBegin          = 65
+    iEnd            = 90
+    iAmountAlpa     = 26
+    sAllcharacters  = ""
+
+    for sChar in sCharacters:
+        if not sChar.isalpha():
+            sAllcharacters += sChar
+        else:
+            if sChar.islower():
+                iBegin = 97
+                iEnd   = 122
+            if ord(sChar) + iCount in range((iBegin - 1), (iEnd + 1)):
+                # When we don't need to go arround we just give the new value
+                sAllcharacters += chr(ord(sChar) + iCount)
+            else:
+                # First ord gives te ascii code of de character than min the begin plus the amount of alpabet and the counter 
+                sAllcharacters += chr(((ord(sChar) - iBegin + iAmountAlpa + iCount) % iAmountAlpa) + iBegin)
+ 
+    print sAllcharacters
+    # print "exersice 3"
+else:
+    print "This is not a valid exercise"
