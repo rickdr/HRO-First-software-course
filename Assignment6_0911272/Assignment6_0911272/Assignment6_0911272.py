c65b7138965801804220c9f53a224bc5d2271ee6
rick droogh <rak_keeper@hotmail.com>
2015-10-06 10:12:57 +0200

    changes

--- Assignment6_0911272/Assignment6_0911272/Assignment6_0911272.py
+++ Assignment6_0911272/Assignment6_0911272/Assignment6_0911272.py
@@ -1,16 +1,16 @@
-﻿print("Witch figure do u wanna see? 1 to 6?")
+﻿import math
+print("Witch figure do u wanna see?\n 1 Is the full square,\n 2 Is the hollow square,\n 3 Is the rectangle triangle,\n 4 Is the pyramid and\n 5 Is the circle ")
 sChoice = input("> ")
 iNumber         = input("Choose a number!")
 sAsterisks      = ""
 
 if sChoice == 1:
-    for i in range (iNumber):
-        for o in range (iNumber):
+    for i in range (0, iNumber):
+        for o in range (0, iNumber):
             sAsterisks += "*"
         sAsterisks += "\n"
 
     print sAsterisks
-
 elif sChoice == 2:
     for i in range (0, iNumber):
         for o in range (0, iNumber):
@@ -26,6 +26,7 @@ elif sChoice == 3:
         for o in range(0, (i+1)):
             sAsterisks += "*"
         sAsterisks += "\n"
+
     print sAsterisks
 elif sChoice == 4:
     iSpaces = iNumber - 1
@@ -36,8 +37,20 @@ elif sChoice == 4:
             sAsterisks += " *"
         iSpaces -= 1   
         sAsterisks += "\n"
+
     print sAsterisks
 elif sChoice == 5:
-    print "5"
+    iRadius = iNumber/2
+    for i in range (0, iNumber+2):
+        for o in range (0, iNumber):
+            iYdist = abs(i - iRadius)
+            iXdist = abs(o - iRadius)
+            if int(math.ceil(math.sqrt(iYdist**2 + iXdist**2))) < iRadius:
+                sAsterisks += "*"
+            else:
+                sAsterisks += " "
+        sAsterisks += "\n"
+
+    print sAsterisks
 else:
     print "No figure in this number!"
