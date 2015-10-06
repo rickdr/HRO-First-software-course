8e22ac26ed67cb3681701951c8ba9e2e7d1a9c48
rick droogh <rak_keeper@hotmail.com>
2015-10-06 22:49:13 +0200

    Some changes and added the face and ball

--- Assignment6_0911272/Assignment6_0911272/Assignment6_0911272.py
+++ Assignment6_0911272/Assignment6_0911272/Assignment6_0911272.py
@@ -1,5 +1,5 @@
 ﻿import math
-print("Witch figure do u wanna see?\n 1 Is the full square,\n 2 Is the hollow square,\n 3 Is the rectangle triangle,\n 4 Is the pyramid and\n 5 Is the circle ")
+print("Witch figure do u wanna see?\n 1 Is the full square\n 2 Is the hollow square\n 3 Is the rectangle triangle\n 4 Is the pyramid and\n 5 Is the circle\n 6 Is the ugly smiley face.")
 sChoice = input("> ")
 iNumber         = input("Choose a number!")
 sAsterisks      = ""
@@ -41,16 +41,37 @@ elif sChoice == 4:
     print sAsterisks
 elif sChoice == 5:
     iRadius = iNumber/2
-    for i in range (0, iNumber+2):
-        for o in range (0, iNumber):
-            iYdist = abs(i - iRadius)
-            iXdist = abs(o - iRadius)
-            if int(math.ceil(math.sqrt(iYdist**2 + iXdist**2))) < iRadius:
+    for i in range (iNumber):
+        for o in range(iNumber):
+            iDistx = i - iRadius
+            iDisty = o - iRadius
+            iDist = math.sqrt((iDisty**2) + (iDistx**2))
+            if iDist < iRadius:
                 sAsterisks += "*"
             else:
                 sAsterisks += " "
         sAsterisks += "\n"
-
+    print sAsterisks
+elif sChoice == 6:
+    iRadius = iNumber/2
+    for i in range (iNumber):
+        for o in range(iNumber):
+            iDistx = i - iRadius
+            iDisty = o - iRadius
+            iDist = math.sqrt((iDisty**2) + (iDistx**2))
+            if iDist < iRadius and iDist > (iNumber/2.6):                
+                sAsterisks += "*"
+            elif (i == iRadius and o == iRadius):
+                sAsterisks += "#"
+            elif (i == iNumber/3 and o == iNumber/3+1) or (i == iNumber/3 and o == iNumber/3*2+1):
+                sAsterisks += "~"
+            elif (i == iNumber/3+1 and o == iNumber/3+1) or (i == iNumber/3+1 and o == iNumber/3*2+1):
+                sAsterisks += "0"
+            elif (i == iNumber/4*3 and iDist < iNumber/3.5):
+                sAsterisks += "-"
+            else:
+                sAsterisks += " "
+        sAsterisks += "\n"
     print sAsterisks
 else:
     print "No figure in this number!"
