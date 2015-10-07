78142119acbbdf371752b2e0fd3d19c2960cb7a7
rick droogh <rak_keeper@hotmail.com>
2015-10-07 16:16:42 +0200

    changes!

---
+++ assignment4_exercise3_0911272/Test1/assignment4_exercise1-2_0911272.py
@@ -0,0 +1,121 @@
+﻿print("Witch exercise do u wanna see?\n 1 is the converter\n 2 is the rock paper scissors\n 3 must be opened by program.py")
+sChoice = input("> ")
+if sChoice == 1:
+   # a:
+    fFahr = float(raw_input("How much Fahrenheit?"))
+    iCelcius = round((fFahr - 32) * 5.0/9.0, 2)
+    print iCelcius
+
+    # b:
+    bValidFahr = False;
+    while bValidFahr == False:
+        iCelcius = input("How much Celsius?")
+        if iCelcius >= -273:
+            Kelvin = round((Celsius + 273), 2)
+            bValidFahr = True;
+        else:
+            Kelvin = "Please give a valid tempature"
+    print Kelvin
+
+    # c:
+    number = input("how much?")
+    if number < 0:
+        number = number * -1;
+    print number
+
+elif sChoice == 2:
+    # a
+    sPlayer1choise      = False
+    sPlayer2choise      = False
+    sPlayer1Question    = "What the choise of player 1? rock, paper, scissors, lizard or spock?"
+    sPlayer2Question    = "What the choise of player 2? rock, paper, scissors, lizard or spock?"
+    sDrawMessage        = "No one wins"
+    # sPlayer1WinMessage  = "Player 1 wins"
+    # sPlayer2WinMessage  = "Player 2 wins"
+    rock_paper          = "Paper covers rock"
+    rock_scissors       = "Rock crushes scissors"
+    rock_lizard         = "Rock crushes lizard"
+    rock_spock          = "Spock vaporizes rock"
+    paper_scissors      = "Scissors cuts paper"
+    paper_lizard        = "Lizard eats paper"
+    paper_spock         = "Paper disproves spock"
+    scissors_lizard     = "Scissors decapitates lizard"
+    scissors_spock      = "Spock smashes scissors"
+    lizard_spock        = "Lizard poisons spock"
+    sMessage            = ""
+
+    while sPlayer1choise == False or sPlayer2choise == False:
+        sPlayer1choise = raw_input(sPlayer1Question).strip('\r')
+        sPlayer2choise = raw_input(sPlayer2Question).strip('\r')
+
+        if sPlayer1choise == "rock":
+            if sPlayer2choise == "rock":
+                sMessage = sDrawMessage
+            elif sPlayer2choise == "paper":
+                sMessage = rock_paper
+            elif sPlayer2choise == "scissors":
+                sMessage = rock_scissors
+            elif sPlayer2choise == "lizard":
+                sMessage = rock_lizard
+            elif sPlayer2choise == "spock":
+                sMessage = rock_spock
+
+        elif sPlayer1choise == "paper":
+            if sPlayer2choise == "rock":
+                sMessage = rock_paper
+            elif sPlayer2choise == "paper":
+                sMessage = sDrawMessage
+            elif sPlayer2choise == "scissors":
+                sMessage = paper_scissors
+            elif sPlayer2choise == "lizard":
+                sMessage = paper_lizard
+            elif sPlayer2choise == "spock":
+                sMessage = paper_spock
+
+        elif sPlayer1choise == "scissors":
+            if sPlayer2choise == "rock":
+                sMessage = rock_scissors
+            elif sPlayer2choise == "paper":
+                sMessage = paper_scissors
+            elif sPlayer2choise == "scissors":
+                sMessage = sDrawMessage
+            elif sPlayer2choise == "lizard":
+                sMessage = scissors_lizard
+            elif sPlayer2choise == "spock":
+                sMessage = scissors_spock
+
+        # b
+        elif sPlayer1choise == "lizard":
+            if sPlayer2choise == "rock":
+                sMessage = rock_lizard
+            elif sPlayer2choise == "paper":
+                sMessage = paper_lizard
+            elif sPlayer2choise == "scissors":
+                sMessage = scissors_lizard
+            elif sPlayer2choise == "lizard":
+                sMessage = sDrawMessage
+            elif sPlayer2choise == "spock":
+                sMessage = lizard_spock
+
+        elif sPlayer1choise == "spock":
+            if sPlayer2choise == "rock":
+                sMessage = rock_spock
+            elif sPlayer2choise == "paper":
+                sMessage = paper_spock
+            elif sPlayer2choise == "scissors":
+                sMessage = scissors_spock
+            elif sPlayer2choise == "lizard":
+                sMessage = lizard_spock
+            elif sPlayer2choise == "spock":
+                sMessage = sDrawMessage
+
+        else:
+            sPlayer1choise = False
+            sPlayer2choise = False
+            print("please try another input!")
+
+    print(sMessage)
+elif sChoice == 3:
+    print("Exercise 3 could be found in folder exercise 3.")
+else:
+    print("Not a valid exercise!")
--- assignment5_0911272/assignment5_0911272/assignment5_0911272.py
+++ assignment5_0911272/assignment5_0911272/assignment5_0911272.py
@@ -1,22 +1,40 @@
 ﻿print "Which exercise do u wanna see? 1, 2 or 3?"
-
 iValue = input(">")
 if iValue == 1:
     sValue = raw_input("What do u wanna reverse?")
-    begin = None
-    einde = None
-    reverse_string = slice(begin, einde, -1)
-    print sValue[reverse_string]
+    sEnd = ""
+
+    for sChar in sValue:
+        sEnd = sChar + sEnd
+
+    print sEnd
+
 elif iValue == 2:
-    import re
-    print "exersice 2"
     sValue = raw_input("Choose a password!")
-    if re.match("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[&'(\-_)=~#{[|`\\^@\]}^$*¨£µ%,;:!?./§+]).{6,50}", sValue):
-        sMessage = "Password is strong!"         
-    elif re.match('(?=.*[a-z])(?=.*[0-9]).{6,50}', sValue):
-        sMessage = "Password is medium"
+    sSpecialChars = "~`!@#$%€^&*()_+=-{}[]|\:;'<>?"
+    iCountDigit = 0
+    iCountUpper = 0
+    iCountSpecial = 0
+
+    iCountChar = len(sValue)
+
+    if iCountChar < 6:
+        sMessage = "password is to short!"
     else:
-        sMessage = "Password is weak!"
+        for sChar in sValue:
+            if sChar.isdigit():
+                iCountDigit = iCountDigit + 1
+            elif sChar.isupper():
+                iCountUpper = iCountUpper + 1
+            elif sChar in sSpecialChars:
+                iCountSpecial = iCountSpecial + 1
+
+        if iCountDigit >= 2 and iCountUpper >= 2 and iCountSpecial >= 1:
+            sMessage = "Password is strong!"         
+        elif iCountDigit >= 1  and (iCountUpper >= 1 or iCountSpecial >= 1):
+            sMessage = "Password is medium"
+        else:
+            sMessage = "Password is weak!"
 
     print sMessage
 elif iValue == 3:
@@ -24,24 +42,22 @@ elif iValue == 3:
     iCount          = input("How much steps do u wanna jump in the alpabet?")
     iBegin          = 65
     iEnd            = 90
-    iAmountAlpa     = 26
+    iAmountAlpabeth = 26
     sAllcharacters  = ""
 
     for sChar in sCharacters:
         if not sChar.isalpha():
             sAllcharacters += sChar
         else:
+            iNew = ord(sChar) + iCount
             if sChar.islower():
                 iBegin = 97
                 iEnd   = 122
-            if ord(sChar) + iCount in range((iBegin - 1), (iEnd + 1)):
-                # When we don't need to go arround we just give the new value
-                sAllcharacters += chr(ord(sChar) + iCount)
-            else:
-                # First ord gives te ascii code of de character than min the begin plus the amount of alpabet and the counter 
-                sAllcharacters += chr(((ord(sChar) - iBegin + iAmountAlpa + iCount) % iAmountAlpa) + iBegin)
- 
+            if iNew > iEnd:
+                iNew -= iAmountAlpabeth
+            elif iNew < iBegin:
+                iNew += iAmountAlpabeth
+            sAllcharacters += chr(iNew)
     print sAllcharacters
-    # print "exersice 3"
 else:
     print "This is not a valid exercise"
